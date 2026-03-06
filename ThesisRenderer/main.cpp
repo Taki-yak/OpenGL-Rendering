8d9a86c804530519adcec0ab12bf5637c7f58c90
Taki <takiyakhlef49@gmail.com>
2026-03-06 09:31:48 +0100

    implemented scene rendering system with multiple objects (cubes)

--- ThesisRenderer/main.cpp
+++ ThesisRenderer/main.cpp
@@ -289,6 +289,19 @@ int main()
     shader.setVec3("viewPos", cameraPos);
     shader.setVec3("lightColor", glm::vec3(1.0f, 1.0f, 1.0f));
     shader.setVec3("objectColor", glm::vec3(1.0f, 1.0f, 1.0f));
+
+    glm::vec3 cubePositions[] = {
+    glm::vec3(0.0f,  0.0f,  0.0f),
+    glm::vec3(2.0f,  5.0f, -15.0f),
+    glm::vec3(-1.5f, -2.2f, -2.5f),
+    glm::vec3(-3.8f, -2.0f, -12.3f),
+    glm::vec3(2.4f, -0.4f, -3.5f),
+    glm::vec3(-1.7f,  3.0f, -7.5f),
+    glm::vec3(1.3f, -2.0f, -2.5f),
+    glm::vec3(1.5f,  2.0f, -2.5f),
+    glm::vec3(1.5f,  0.2f, -1.5f),
+    glm::vec3(-1.3f,  1.0f, -1.5f)
+    };
   
     // ================= RENDER LOOP =================
 
@@ -340,7 +353,22 @@ int main()
        // shader.setVec3("objectColor", glm::vec3(1.0f, 0.5f, 0.3f));
 
         glBindVertexArray(VAO);
-        glDrawArrays(GL_TRIANGLES, 0, 36);
+        for (unsigned int i = 0; i < 10; i++)
+        {
+            glm::mat4 model = glm::mat4(1.0f);
+
+            model = glm::translate(model, cubePositions[i]);
+
+            float angle = 20.0f * i;
+
+            model = glm::rotate(model,
+                glm::radians(angle) + (float)glfwGetTime(),
+                glm::vec3(1.0f, 0.3f, 0.5f));
+
+            shader.setMat4("model", glm::value_ptr(model));
+
+            glDrawArrays(GL_TRIANGLES, 0, 36);
+        }
 
 
         // ===== DRAW LIGHT CUBE =====
